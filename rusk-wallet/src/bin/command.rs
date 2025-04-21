dusk-network/rusk

rusk-wallet: move restore from seed logic into `Command::restore_from_seed`

rusk-wallet/src/bin/command.rs
@@ -737,6 +737,31 @@ impl Command {
 
         Ok(w)
     }
+
+    pub fn run_restore_from_seed(wallet_path: &WalletPath, prompter: &dyn Prompt) -> anyhow::Result<Wallet<WalletFile>> {
+        // ask user for 12-word mnemonic phrase
+        let phrase = prompt::request_mnemonic_phrase(prompter)?;
+        let salt = gen_salt();
+        let iv = gen_iv();
+        // ask user for a password to secure the wallet, create the latest
+        // wallet file from the seed
+        let key = prompt::derive_key_from_new_password(
+            &None,
+            Some(&salt),
+            dat::DatFileVersion::RuskBinaryFileFormat(LATEST_VERSION),
+            prompter,
+        )?;
+        // create and store the recovered wallet
+        let mut w = Wallet::new(phrase)?;
+        let path = wallet_path.clone();
+        w.save_to(WalletFile {
+            path,
+            aes_key: key,
+            salt: Some(salt),
+            iv: Some(iv),
+        })?;
+        Ok(w)
+    }
 }
 
 /// Possible results of running a command in interactive mode

rusk-wallet/src/bin/interactive.rs
@@ -10,7 +10,7 @@ use std::fmt::Display;
 
 use inquire::{InquireError, Select};
 use rusk_wallet::currency::Dusk;
-use rusk_wallet::dat::{DatFileVersion, LATEST_VERSION};
+use rusk_wallet::dat::DatFileVersion;
 use rusk_wallet::{
     Address, Error, Profile, Wallet, WalletPath, IV_SIZE, MAX_PROFILES,
     SALT_SIZE,
@@ -19,7 +19,7 @@ use rusk_wallet::{
 use crate::io::{self, prompt};
 use crate::prompt::Prompter;
 use crate::settings::Settings;
-use crate::{gen_iv, gen_salt, Command, GraphQL, RunResult, WalletFile};
+use crate::{Command, GraphQL, RunResult, WalletFile};
 
 /// Run the interactive UX loop with a loaded wallet
 pub(crate) async fn run_loop(
@@ -252,33 +252,14 @@ pub(crate) async fn load_wallet(
             }
         }
         // Use the latest binary format when creating a wallet
-        MainMenu::Create => {
-            Command::run_create(false, &None, password, &wallet_path, &Prompter)?
-        }
-        MainMenu::Recover => {
-            // ask user for 12-word mnemonic phrase
-            let phrase = prompt::request_mnemonic_phrase()?;
-            let salt = gen_salt();
-            let iv = gen_iv();
-            // ask user for a password to secure the wallet, create the latest
-            // wallet file from the seed
-            let key = prompt::derive_key_from_new_password(
-                &None,
-                Some(&salt),
-                DatFileVersion::RuskBinaryFileFormat(LATEST_VERSION),
-                &Prompter,
-            )?;
-            // create and store the recovered wallet
-            let mut w = Wallet::new(phrase)?;
-            let path = wallet_path.clone();
-            w.save_to(WalletFile {
-                path,
-                aes_key: key,
-                salt: Some(salt),
-                iv: Some(iv),
-            })?;
-            w
-        }
+        MainMenu::Create => Command::run_create(
+            false,
+            &None,
+            password,
+            &wallet_path,
+            &Prompter,
+        )?,
+        MainMenu::Recover => Command::run_restore_from_seed(wallet_path, &Prompter)?,
         MainMenu::Exit => std::process::exit(0),
     };
 

lumos-codes-dev/dusk-op-l1-api-rusk-node

rusk-wallet/src/bin/command.rs
@@ -742,6 +742,31 @@ impl Command {
 
         Ok(w)
     }
+
+    pub fn run_restore_from_seed(wallet_path: &WalletPath, prompter: &dyn Prompt) -> anyhow::Result<Wallet<WalletFile>> {
+        // ask user for 12-word mnemonic phrase
+        let phrase = prompt::request_mnemonic_phrase(prompter)?;
+        let salt = gen_salt();
+        let iv = gen_iv();
+        // ask user for a password to secure the wallet, create the latest
+        // wallet file from the seed
+        let key = prompt::derive_key_from_new_password(
+            &None,
+            Some(&salt),
+            dat::FileVersion::RuskBinaryFileFormat(LATEST_VERSION),
+            prompter,
+        )?;
+        // create and store the recovered wallet
+        let mut w = Wallet::new(phrase)?;
+        let path = wallet_path.clone();
+        w.save_to(WalletFile {
+            path,
+            aes_key: key,
+            salt: Some(salt),
+            iv: Some(iv),
+        })?;
+        Ok(w)
+    }
 }
 
 /// Possible results of running a command in interactive mode

rusk-wallet/src/bin/interactive.rs
@@ -10,7 +10,7 @@ use std::fmt::Display;
 
 use inquire::{InquireError, Select};
 use rusk_wallet::currency::Dusk;
-use rusk_wallet::dat::{FileVersion as DatFileVersion, LATEST_VERSION};
+use rusk_wallet::dat::FileVersion;
 use rusk_wallet::{
     Address, Error, Profile, Wallet, WalletPath, IV_SIZE, MAX_PROFILES,
     SALT_SIZE,
@@ -19,7 +19,7 @@ use rusk_wallet::{
 use crate::io::{self, prompt};
 use crate::prompt::Prompter;
 use crate::settings::Settings;
-use crate::{gen_iv, gen_salt, Command, GraphQL, RunResult, WalletFile};
+use crate::{Command, GraphQL, RunResult, WalletFile};
 
 /// Run the interactive UX loop with a loaded wallet
 pub(crate) async fn run_loop(
@@ -213,7 +213,7 @@ pub(crate) async fn load_wallet(
     wallet_path: &WalletPath,
     settings: &Settings,
     file_version_and_salt_iv: Result<
-        (DatFileVersion, Option<(Salt, Iv)>),
+        (FileVersion, Option<(Salt, Iv)>),
         Error,
     >,
 ) -> anyhow::Result<Wallet<WalletFile>> {
@@ -252,33 +252,14 @@ pub(crate) async fn load_wallet(
             }
         }
         // Use the latest binary format when creating a wallet
-        MainMenu::Create => {
-            Command::run_create(false, &None, password, &wallet_path, &Prompter)?
-        }
-        MainMenu::Recover => {
-            // ask user for 12-word mnemonic phrase
-            let phrase = prompt::request_mnemonic_phrase()?;
-            let salt = gen_salt();
-            let iv = gen_iv();
-            // ask user for a password to secure the wallet, create the latest
-            // wallet file from the seed
-            let key = prompt::derive_key_from_new_password(
-                &None,
-                Some(&salt),
-                DatFileVersion::RuskBinaryFileFormat(LATEST_VERSION),
-                &Prompter,
-            )?;
-            // create and store the recovered wallet
-            let mut w = Wallet::new(phrase)?;
-            let path = wallet_path.clone();
-            w.save_to(WalletFile {
-                path,
-                aes_key: key,
-                salt: Some(salt),
-                iv: Some(iv),
-            })?;
-            w
-        }
+        MainMenu::Create => Command::run_create(
+            false,
+            &None,
+            password,
+            &wallet_path,
+            &Prompter,
+        )?,
+        MainMenu::Recover => Command::run_restore_from_seed(wallet_path, &Prompter)?,
         MainMenu::Exit => std::process::exit(0),
     };
 